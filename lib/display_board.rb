1e94732b73d6504a67b2da8c744d0de347ead6a3
Kazumi Karbowski <kazkazinus@gmail.com>
2018-02-03 14:55:03 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,5 @@
-# Define a method display_board that prints a 3x3 Tic Tac Toe Board
+# Define a method display_board that prints a 3x3 Tic Tac Toe Boarddef display_board
 def display_board
-  puts "A Tic Tac Toe Board"
+  2.times {puts "   |   |   ", "-----------"}
+  puts "   |   |   "
 end
-
-board = ["   ", "   ","   ","   ","   ","   ","   ","   ","   "]
